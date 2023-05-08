ad2ae71e001b83912962bb771490ba129fea147f
nikodittmar <nikodittmar@gmail.com>
2023-05-08 09:49:19 -0700

    made api keys private

---
+++ api_keys.js
@@ -0,0 +1,3 @@
+// TODO(developer): Set to client ID and API key from the Developer Console
+const CLIENT_ID = '934864645785-rqa4jqsq209733epdg6gr3f3ecbgd0ll.apps.googleusercontent.com';
+const API_KEY = 'AIzaSyDlfBxR_EpNPZramtrWdS4m_oAEHnGlMxo';
